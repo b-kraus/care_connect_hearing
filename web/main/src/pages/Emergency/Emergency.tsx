b7f5cb99f7cd98014340205e0b3a2e6e586fa115
Camilla Mekonnen <Camillamulusew@gmail.com>
2026-07-28 19:52:29 -0400

    Fix build errors, deploy to Netlify

--- web/main/src/pages/Emergency/Emergency.tsx
+++ web/main/src/pages/Emergency/Emergency.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { AlertTriangle, Check, X, ChevronsRight } from "lucide-react";
 
